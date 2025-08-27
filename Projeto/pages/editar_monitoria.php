28d884df426070097992260ba079c78fa20b08eb
Gabriel Luis <176835585+Gabriel-Luis123@users.noreply.github.com>
2025-08-27 10:42:36 -0300

    Add files via upload

--- Projeto/pages/editar_monitoria.php
+++ Projeto/pages/editar_monitoria.php
@@ -6,14 +6,11 @@ include_once 'header.php';
 ?>
 
 <main class="card-grid">
-    <div class="card-grid-sair">
-        <a href="minhas_monitorias.php" id="linkSair">x</a> 
-    </div>
-    <form class="container-formulario" id="form-monitoria" method="POST" action="minhas_monitorias.php">
+    <form class="container-formulario" id="form-monitoria" method="POST" action="#">
         <div class="cabecalho-formulario">
-            <div class="cabecalho-formulario-nome">Ricardo Dias</div>
-            <div class="cabecalho-formulario-disciplina">Matemática</div>
-            <div class="cabecalho-formulario-info">INFO 2</div>
+            <div class="cabecalho-formulario-nome cabecalho-destaque">Ricardo Dias</div>
+            <div class="cabecalho-formulario-disciplina cabecalho-destaque  ">Matemática</div>
+            <div class="cabecalho-formulario-info cabecalho-destaque">INFO 2</div>
         </div>
         <div class="corpo-formulario">
             <div class="linha-formulario">
@@ -22,15 +19,11 @@ include_once 'header.php';
             </div>
             <div class="linha-formulario">
                 <label for="inputSala">Sala:</label>
-                <input type="text" id="inputSala" value="302">
+                <input type="text" id="inputSala" placeholder="302">
             </div>
             <div class="linha-formulario">
                 <label for="inputHorario">Horário:</label>
-                <input type="text" id="inputHorario" value="19:30">
-            </div>
-            <div class="linha-formulario">
-                <label for="inputMaterias">Conteúdos:</label>
-                <input type="text" id="inputMaterias" value="Geometria">
+                <input type="text" id="inputHorario" placeholder="00:00">
             </div>
             <div class="linha-formulario largura-total">
                 <textarea id="areaTextoMateriais"
@@ -38,7 +31,7 @@ include_once 'header.php';
             </div>
         </div>
         <div class="rodape-formulario">
-            <button class="botao-enviar" id="botaoConfirmarEdicao" type="submit">✔️</button>
+            <button class="botao-enviar" id="botaoConfirmarEdicao">✔</button>
         </div>
     </form>
 
--- Projeto/pages/minhas_monitorias_inscritas.php
+++ Projeto/pages/minhas_monitorias_inscritas.php
@@ -1,6 +1,6 @@
 <?php
-$titlePage = 'Minhas Monitorias';
-$nameCSS = "minhas_monitorias";
+$titlePage = 'Minhas Monitorias Inscritas';
+$nameCSS = "minhas_monitorias_inscritas";
 
 include_once 'header.php';
 ?>
@@ -19,15 +19,12 @@ include_once 'header.php';
         <!-- Cards repetidos -->
         <div class="card">
                 <div class="card-top">
-                    <button class="edit-icon" id="iconeEdicao">
-                        <img src="../public/img/formsComponents/icone-edicao.jpeg" alt="Ícone de Edição" class="edit-image">
-                    </button>
                     <div class="options-icon" id="optionsIcon">⋮</div>
 
                     <div class="options-popup" id="optionsPopup">
                         <ul>
                             <li><a href="#" class="popup-option" data-action="editar">Editar</a></li>
-                            <li><a href="#" class="popup-option" data-action="excluir" id="linkSair">Excluir</a></li>
+                            <li><a href="#" class="popup-option" data-action="excluir" id="linkSair">Desinscrever</a></li>
                         </ul>
                     </div>
                 </div>
@@ -52,7 +49,7 @@ include_once 'header.php';
     </button>
     <div class="modal-overlay-sair" id="confirmModal-sair">
         <div class="modal-box-sair">
-            <h3>Deseja realmente excluir as informações da monitoria selecionada?</h3>
+            <h3>Deseja realmente desinscrever da monitoria selecionada?</h3>
             <button class="btn-confirmar-sair" id="btnSim-sair">Confirmar</button>
             <button class="btn-cancelar-sair" id="btnNao-sair">Cancelar</button>
         </div>
@@ -62,7 +59,7 @@ include_once 'header.php';
 <?php
 
 
-$scripts = ["minhas_monitoriaJS/minhas_monitorias", "minhas_monitoriaJS/minhas_monitorias_excluir"];
+$scripts = ["minhas_monitoriaJS/minhas_monitorias_inscritas", "minhas_monitoriaJS/minhas_monitorias_excluir"];
 include_once 'footer.php';
 
 ?>
